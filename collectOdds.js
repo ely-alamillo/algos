7de482623c32379d4ea3d181b1ef9b06e354b0a4
Ely Alamillo <ely0013@gmail.com>
2019-05-09 20:51:16 -0500

    add collectOdds

--- collectOdds.js
+++ collectOdds.js
@@ -18,4 +18,20 @@ const collectOdds = (nums) => {
   return result
 }
 
+// example of above program, only uses
+// pure recursion. note concat
+const pureCollectOdds = (nums) => {
+  let newNums = []
+
+  if (nums.length === 0) return newNums
+  if (nums[0] % 2 !== 0) {
+    newNums.push(nums[0])
+  }
+
+  newNums = newNums.concat(pureCollectOdds(nums.slice(1)))
+
+  return newNums
+}
+
 console.log(collectOdds([1, 2, 3, 4, 5, 6, 7, 8, 9])) // [1,3,5,7,9]
+console.log(pureCollectOdds([1, 2, 3, 4, 5, 6, 7, 8, 9])) // [1,3,5,7,9]
