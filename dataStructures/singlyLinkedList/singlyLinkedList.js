72604f2601a9916d4fbab3fce9a731ad896daaeb
Ely Alamillo <ely0013@gmail.com>
2019-05-23 20:52:39 -0500

    add more methods

--- dataStructures/singlyLinkedList/singlyLinkedList.js
+++ dataStructures/singlyLinkedList/singlyLinkedList.js
@@ -1,3 +1,7 @@
+// insertion O(1)
+// removal O(1) or O(n)
+// searching O(n)
+// access O(n)
 class Node {
   constructor(value) {
     this.value = value;
@@ -94,6 +98,91 @@ class SinglyLinkedList {
 
     return this;
   }
+
+  get(position) {
+    if (position >= this.length || position < 0) {
+      return null;
+    }
+    let count = 0;
+    let current = this.head;
+    while (count !== position) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(position, value) {
+    const node = this.get(position);
+    if (!node) return false;
+    node.value = value;
+    return true;
+  }
+
+  insert(position, value) {
+    // not valid
+    if (position < 0 || position > this.length) return false;
+    // insert at end
+    if (position === this.length) return !!this.push(value);
+    // insert at front
+    if (position === 0) return !!this.unshift(value);
+
+    const newNode = new Node(value);
+    const prev = this.get(position - 1);
+    const tempNode = prev.next;
+    prev.next = newNode;
+    newNode.next = tempNode;
+    this.length++;
+
+    return true;
+  }
+
+  remove(position) {
+    if (position < 0 || position >= this.length) return false;
+    if (position === 0) return this.shift;
+    if (position === this.length - 1) return this.pop;
+
+    const prevNode = this.get(position - 1);
+    const removed = prevNode.next;
+    prevNode.next = removed.next;
+    this.length--;
+
+    return removed;
+  }
+
+  reverse() {
+    let currNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currNode) {
+      // Store next node.
+      nextNode = currNode.next;
+
+      // Change next node of the current node so it would link to previous node.
+      currNode.next = prevNode;
+
+      // Move prevNode and currNode nodes one step forward.
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+
+    // Reset head and tail.
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
+
+  print() {
+    const arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    console.log('current list items: ' + arr);
+  }
 }
 
 const list = new SinglyLinkedList();
@@ -108,3 +197,20 @@ list.push('two');
 console.log(list.shift());
 console.log(list); // {head: { value: "two", next: null}, tail: { value: "two", next: null}, length: 1}
 console.log(list.unshift('new head')); // {head: { value: "new head", next: ...rest}, tail: { value: "two", next: null}, length: 2}
+console.log(list.get(0)); // {value: "new head", next: ..rest}
+console.log(list.set(0, 'new new head')); // true
+console.log(list.get(0)); // {value: "new new head", next: ..rest}
+console.log(list.set(-1)); // false
+console.log(list.set(10)); // false
+console.log(list.insert(0, 'im the new first')); // true
+console.log(list);
+console.log('--- reverse starts here ---');
+const newList = new SinglyLinkedList();
+newList.push(100);
+newList.push(200);
+newList.push(300);
+newList.push(400);
+newList.print();
+newList.reverse();
+console.log('reversing list ...');
+newList.print();
